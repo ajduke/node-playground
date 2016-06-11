04d83a21ffe72b4505b704a3da0fafdd7716be7e
ajduke <ajduke@about.me>
2016-06-11 18:23:29 +0530

    wired the project with mongodb

--- app2/routes/users.js
+++ app2/routes/users.js
@@ -1,29 +1,54 @@
 var express = require('express');
+var mongojs = require('mongojs');
 var router = express.Router();
 
+var db = mongojs('localhost:27017/starbucks', ['users']);
+
 var Users = [];
+router.get('/', function(req, res, next){
+  res.send({
+    message: 'root of API'
+  })
+});
 
 router.get('/user', function(req, res, next) {
-  res.status(200).send({
-    users: Users
+  db.users.find({},{user_id: 1}, function(err, doc){
+    res.status(200).send({
+      status: 200,
+      data: {
+        users: doc
+      }
+
+    });
   });
+
 });
 
 
 router.post('/user', function(req, res, next){
-
   var username = req.body.username;
   var user_id = req.body.user_id;
   var obj = {
     user_id: user_id,
     username: username
   };
-  Users.push(obj);
-  res.send({
-    status: 200,
-    method: 'post',
-    users: Users
-  });
+
+  db.users.insert(obj, function(err, doc){
+    if(err) {
+      res.status(500).end({
+        status: 500,
+        users: doc
+      });
+    } else {
+      res.send({
+        status: 200,
+        users: doc
+      });
+    }
+  })
+
+
+
 });
 
 
@@ -31,35 +56,39 @@ router.put('/user', function(req, res, next){
   var user_id = req.body.user_id;
   var username = req.body.username;
 
-  for(var u in Users){
-    var usr = Users[u];
-    if(usr.user_id === user_id){
-      usr.username = username;
-      break;
+
+  db.users.update({user_id: user_id}, {$set:{userame: username}}, function(err, doc){
+    if(err){
+      console.log("Error ", err.code);
+      res.status(500).send({
+        status: 500,
+        data: doc
+      })
+      return
     }
-  }
-  res.send({
-    status: 200,
-    message: 'updated the user...'
+
+    res.send({
+      status: 200,
+      message: 'updated the user...',
+      doc: doc
+    })
   })
+
 });
 
 
 router.delete('/user', function(req, res, next){
   var user_id = req.body.user_id;
-  var username = req.body.username;
-
-  var indx = -1;
-  for(var u in Users){
-    var usr = Users[u];
-    if(usr.user_id === user_id){
-      indx = u;
-      break;
+  db.users.remove({user_id: user_id}, function(err, doc){
+    if(err){
+      console.log("Error ", err.code);
+      res.status(500).send({
+        status: 500,
+        data: doc
+      })
+      return
     }
-  }
-  if(indx !== -1 ){
-    Users.splice(indx, 1);
-  }
+  })
   res.send({
     status: 200,
     method:'delete'
